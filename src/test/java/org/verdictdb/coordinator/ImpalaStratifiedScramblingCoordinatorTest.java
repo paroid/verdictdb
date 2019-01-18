03542644faba4a77d8bcee1635367084728a78ec
91017 <joezhong@umich.edu>
2019-01-18 17:31:55 -0500

    Add unit test for other DB

---
+++ src/test/java/org/verdictdb/coordinator/ImpalaStratifiedScramblingCoordinatorTest.java
@@ -0,0 +1,133 @@
+package org.verdictdb.coordinator;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.tuple.Pair;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.verdictdb.commons.DatabaseConnectionHelpers;
+import org.verdictdb.connection.DbmsConnection;
+import org.verdictdb.connection.DbmsQueryResult;
+import org.verdictdb.connection.JdbcConnection;
+import org.verdictdb.exception.VerdictDBDbmsException;
+import org.verdictdb.exception.VerdictDBException;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class ImpalaStratifiedScramblingCoordinatorTest {
+
+  private static Connection impalaConn;
+
+  private static Statement impalaStmt;
+
+  private static final String IMPALA_HOST;
+
+  // to avoid possible conflicts among concurrent tests
+  private static final String IMPALA_DATABASE =
+      "stratified_scrambling_coordinator_test_" + RandomStringUtils.randomAlphanumeric(8).toLowerCase();
+
+  private static final String IMPALA_USER = "";
+
+  private static final String IMPALA_PASSWORD = "";
+
+  static {
+    IMPALA_HOST = System.getenv("VERDICTDB_TEST_IMPALA_HOST");
+  }
+
+  @BeforeClass
+  public static void setupImpalaDatabase()
+      throws SQLException, VerdictDBDbmsException, IOException {
+    String impalaConnectionString = String.format("jdbc:impala://%s", IMPALA_HOST);
+    impalaConn =
+        DatabaseConnectionHelpers.setupImpala(
+            impalaConnectionString, IMPALA_USER, IMPALA_PASSWORD, IMPALA_DATABASE);
+  }
+
+  @AfterClass
+  public static void tearDown() throws SQLException {
+    impalaConn
+        .createStatement()
+        .execute(String.format("DROP SCHEMA IF EXISTS `%s` CASCADE", IMPALA_DATABASE));
+    impalaConn.close();
+  }
+
+  @Test
+  public void sanityCheck() throws VerdictDBDbmsException {
+    DbmsConnection dbmsConn = JdbcConnection.create(impalaConn);
+
+    DbmsQueryResult rs =
+        dbmsConn.execute(String.format("select * from %s.lineitem", IMPALA_DATABASE));
+    assertEquals(1000, rs.getRowCount());
+  }
+
+  @Test
+  public void testScramblingCoordinatorLineitem() throws VerdictDBException {
+    testScramblingCoordinator("lineitem", "l_quantity");
+  }
+
+  @Test
+  public void testScramblingCoordinatorOrders() throws VerdictDBException {
+    testScramblingCoordinator("lineitem", "l_discount");
+  }
+
+  public void testScramblingCoordinator(String tablename, String columnname) throws VerdictDBException {
+    JdbcConnection conn = JdbcConnection.create(impalaConn);
+    //    conn.setOutputDebugMessage(true);
+
+    String scrambleSchema = IMPALA_DATABASE;
+    String scratchpadSchema = IMPALA_DATABASE;
+    long blockSize = 100;
+    ScramblingCoordinator scrambler =
+        new ScramblingCoordinator(conn, scrambleSchema, scratchpadSchema, blockSize);
+
+    // perform scrambling
+    String originalSchema = IMPALA_DATABASE;
+    String originalTable = tablename;
+    String scrambledTable = tablename + "_scrambled";
+    conn.execute(String.format("drop table if exists %s.%s", IMPALA_DATABASE, scrambledTable));
+    scrambler.scramble(originalSchema, originalTable, originalSchema, scrambledTable, "stratified", columnname);
+
+    // tests
+    List<Pair<String, String>> originalColumns = conn.getColumns(IMPALA_DATABASE, originalTable);
+    List<Pair<String, String>> columns = conn.getColumns(IMPALA_DATABASE, scrambledTable);
+    for (int i = 0; i < originalColumns.size(); i++) {
+      assertEquals(originalColumns.get(i).getLeft(), columns.get(i).getLeft());
+      assertEquals(originalColumns.get(i).getRight(), columns.get(i).getRight());
+    }
+    assertEquals(originalColumns.size()+2, columns.size());
+
+    List<String> partitions = conn.getPartitionColumns(IMPALA_DATABASE, scrambledTable);
+    assertEquals(Arrays.asList("verdictdbblock"), partitions);
+
+    DbmsQueryResult result1 =
+        conn.execute(String.format("select count(*) from %s.%s", IMPALA_DATABASE, originalTable));
+    DbmsQueryResult result2 =
+        conn.execute(String.format("select count(*) from %s.%s", IMPALA_DATABASE, scrambledTable));
+    result1.next();
+    result2.next();
+    assertEquals(result1.getInt(0), result2.getInt(0));
+
+    DbmsQueryResult result =
+        conn.execute(
+            String.format("select min(verdictdbblock), max(verdictdbblock) from %s.%s",
+                IMPALA_DATABASE, scrambledTable));
+    result.next();
+    assertEquals(0, result.getInt(0));
+    //assertEquals((int) Math.ceil(result2.getInt(0) / (float) blockSize) - 1, result.getInt(1));
+
+    // Rare groups are large enough. Around 50% of first block should be tier0.
+    DbmsQueryResult tierResult =
+        conn.execute(
+            String.format("select count(*) from %s.%s where verdictdbblock=0 and verdictdbtier=0",
+                IMPALA_DATABASE, scrambledTable));
+    tierResult.next();
+    assertEquals(0.5, tierResult.getDouble(0) / blockSize, 0.1);
+  }
+}
